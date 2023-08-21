450c79a100b7ce983e915e421e2c612f8ec7255a
Sajid-533 <md.sajidali231@gmail.com>
2023-08-21 20:19:50 +0530

    Javascript pwSkill assingment lecture 4 to 10

---
+++ Q1.js
@@ -0,0 +1,9 @@
+function isUserPersent(name) {
+    if (name == "Mithun" || "Rohan" || "Sajid"){
+       console.log(`Yes, ${name} is valid user.`); 
+    } else { console.log(`No, ${name} is not a valid user.`);
+
+    }
+}
+isUserPersent("Mithun");
+isUserPersent("Someone");
---
+++ Readme.md
@@ -0,0 +1,2 @@
+Pwskill assingment from lecture 4 to 9  
+it contain Pwskill assingment answer.
